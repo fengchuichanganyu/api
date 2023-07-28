8d040bd1ec5b6c16d50fd33b38683978b877639a
giteestudyroom <1132980470@qq.com>
2023-07-28 17:55:42 +0800

    04-添加路由（处理不同请求的url）

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-const Koa = require('koa')
+const app = require('./app')
+
 const {APP_PORT}= require('./config/config.default')
-const app= new Koa()
-app.use ((ctx,next)=>{
-ctx.body='hello api'
-})
+
 app.listen(APP_PORT,()=>{
     console.log(`server is running on http://localhost:${APP_PORT}`)
 })
