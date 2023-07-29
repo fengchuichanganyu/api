0f611cc18b94ebb2ec219b9306dd95551a0dff32
giteestudyroom <1132980470@qq.com>
2023-07-29 18:08:47 +0800

    09添加用户

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,7 +1,16 @@
+const User = require('../model/user.model')
+
 class UserService{
     async createUser(user_name,password){
-        //todo:写入数据库
-        return '写入数据库成功'
-    }
+        //插入数据
+        // User.create({
+        //     user_name:user_name ,
+        //     password:password
+        // })
+
+     const res = await User.create({user_name,password}) 
+    //  console.log(res)
+    return res.dataValues
+  }
 }
 module.exports = new UserService
