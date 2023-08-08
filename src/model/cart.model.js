406096a4be82b8a5a6b5a9ced94814ea16790b13
giteestudyroom <1132980470@qq.com>
2023-08-08 21:07:55 +0800

    商品添加到购物车下

---
+++ src/model/cart.model.js
@@ -0,0 +1,36 @@
+const { DataTypes} = require('sequelize')
+const seq = require('../db/seq')
+
+const Cart = seq.define('zd_carts',{
+    goods_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        comment:''
+    },
+    goods_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        comment:'商品id'
+    },
+    user_id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        comment:'用户id'
+    },
+    number:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue:1,
+        comment:'商品数量'
+    },
+    selected:{
+        type:DataTypes.BOOLEAN,
+        allowNull:false,
+        defaultValue:true,
+        comment:'是否选中',
+    }
+})
+
+// Cart.sync({force:true})
+
+module.exports = Cart
--- src/service/carts.service.js
+++ src/service/carts.service.js
@@ -1,8 +1,27 @@
+const { Op } = require('sequelize')
+const Cart = require('../model/cart.model')
+
 class CartService{
     async createdOrUpdate(user_id,goods_id){
-        return {
-            code:'0',
+      let res =  await Cart.findOne({
+            where:{
+                [Op.and]:{
+                    user_id,
+                    goods_id,
+                },
+            },
+        })
+        
+        if(res){
+            await res.increment('number')
+            console.log(res)
 
+           return await res.reload()
+        }else{
+             return await Cart.create({
+                user_id,
+                goods_id
+             })
         }
     }
 }
