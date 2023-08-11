98e517b7a9a7b04fa9192aa334016628fc84a278
giteestudyroom <1132980470@qq.com>
2023-08-11 12:15:35 +0800

    添加地址（完）

--- src/router/addr.route.js
+++ src/router/addr.route.js
@@ -4,14 +4,12 @@ const router  = new Router({prefix:'/address'})
 
 const {auth} = require('../middleware/auth.middleware')
 const {validator} = require('../middleware/addr.middleware')
+const {create} = require('../controller/addr.controller')
 
 router.post('/', auth,validator({
     consignee:'string',
     phone:{ type :'string', format: /^1\d{10}$/ },
     address:"string",
-}),
- (ctx)=>{
-    ctx.body ='添加成功'
-})
+}),create)
 
 module.exports = router
