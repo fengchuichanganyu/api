bc99011f835d2b9621f100e9294b24bb9706ffe3
giteestudyroom <1132980470@qq.com>
2023-08-11 16:32:34 +0800

    获取更新以及删除和设置默认地址

--- src/controller/addr.controller.js
+++ src/controller/addr.controller.js
@@ -1,4 +1,4 @@
-const {createAddr} = require('../service/addr.service')
+const {createAddr,findAllAddr,updateAddr,removeAddr,setDefaultAddr} = require('../service/addr.service')
 
 class AddrController{
     async create(ctx){
@@ -12,6 +12,51 @@ class AddrController{
             message:'添加地址成功',
             result:res,
         }
+    }
+
+    async findAll(ctx){
+        const user_id = ctx.state.user.id
+        const res = await findAllAddr(user_id)
+
+        ctx.body = {
+            code:0,
+            message:'获取地址列表成功',
+            result:res,
+        }
+    }
+
+    async update(ctx){
+        const id = ctx.request.params.id
+       const res =  await updateAddr(id,ctx.request.body)
+
+       ctx.body = {
+        code:0,
+        message:'更新地址成功',
+        result:res,
+       }
+    }
+
+    async remove(ctx){
+        const id = ctx.request.params.id
+       const res =  await removeAddr(id)
+
+       ctx.body = {
+        code:0,
+        message:'删除地址成功',
+        result:res,
+       }
+    }
+    async setDefault(ctx){
+        const id = ctx.request.params.id
+        const user_id = ctx.state.user.id
+
+        const res =  await setDefaultAddr(id,user_id)
+console.log(res)
+       ctx.body = {
+        code:0,
+        message:'设置默认地址成功',
+       result:res
+       }
     }
 }
 
--- src/service/addr.service.js
+++ src/service/addr.service.js
@@ -4,6 +4,41 @@ class AddrService{
     async createAddr(addr){
        return await Address.create(addr)
     }
+
+    async findAllAddr(user_id){
+        return await Address.findAll({
+            attributes:['id','consignee','address','is_default'],
+            where:{user_id}
+        })
+    }
+
+    async updateAddr(id,addr){
+        return await Address.update(addr, { where: { id } } )
+    }
+
+    async removeAddr(id){
+        console.log(id)
+        return await Address.destroy({where:{ id } })
+    }
+
+    async setDefaultAddr(id,user_id){
+        await Address.update(
+            {is_default:0},
+            {
+                where:{
+                    user_id,
+                },
+            }
+        )
+       return await Address.update(
+            {is_default:1},
+            {
+                where:{
+                    id,
+                },
+            }
+        )
+    }
 }
 
 module.exports = new AddrService()
